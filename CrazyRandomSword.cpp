0c55a622b4e655f30c6587e185c3c73544bed2aa
Jason Johansson <jpj16@my.fsu.edu>
2019-01-29 13:28:42 -0500

    Implemented CrazyRandomSword class prototype and functions

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -1,1 +1,9 @@
-#pragma once
+#include "CrazyRandomSword.h"
+
+double CrazyRandomSword::hit(double armor) {
+	srand(time(NULL));
+	double dmg = hitPoints - (rand()%(static_cast<int>(armor/3))) + 2;
+	if (dmg < 0)
+		return 0;
+	return dmg;
+}
--- CrazyRandomSword.h
+++ CrazyRandomSword.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef CRAZYRANDOMSWORD_H
+#define CRAZYRANDOMSWORD_H
+
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include "Weapon.h"
+
+class CrazyRandomSword : public Weapon {
+public:
+	CrazyRandomSword() : Weapon("CrazyRandom Sword", (rand()%93) + 7 ){
+		srand(time(NULL));
+	}
+	virtual ~CrazyRandomSword() {};
+	virtual double hit(double armor);
+};
+
+#endif // !CRAZYRANDOMSWORD_H
