ae7ec79d660845f980ed3bcea6d8849501f721a0
Jason Johansson <jpj16@my.fsu.edu>
2019-01-29 13:38:03 -0500

    Implemented Dagger class prototype and functions

--- Dagger.cpp
+++ Dagger.cpp
@@ -1,1 +1,13 @@
-#pragma once
+//Jason Johansson
+//Dagger.cpp
+#include "Dagger.h"
+#include <cstdlib>
+#include <ctime>
+
+double Dagger::hit(double armor) {
+	srand(time(NULL));
+	double dmg = hitPoints - (rand() % static_cast<int>(armor)) + (armor*0.75);
+	if (dmg < 0)
+		return 0;
+	return dmg;
+}
--- Dagger.h
+++ Dagger.h
@@ -1,1 +1,18 @@
-#pragma once
+//Jason Johansson
+//Dagger.h
+//Dagger hits for 10dmg and ignores 75-100% of armor
+
+#ifndef DAGGER_H
+#define DAGGER_H
+
+#include <string>
+#include "Weapon.h"
+
+class Dagger : public Weapon {
+public: 
+	Dagger() : Weapon("Dagger", 10.0) {}
+	virtual ~Dagger() {};
+	virtual double hit(double armor);
+};
+
+#endif
