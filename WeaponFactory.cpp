720daaa5b76c7dbd4ad5a6371d841486a1af2f03
Jason Johansson <jpj16@my.fsu.edu>
2019-01-29 13:43:23 -0500

    Updated WeaponFactory.cpp to implement new weapons: SimpleHammer, CrazyRandomSword, Dagger

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "Dagger.h"
+#include "CrazyRandomSword.h"
+#include "SimpleHammer.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -23,10 +26,16 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     if (name.compare("sword") == 0) {
         return new CommonSword();
     }
-    
     if (name.compare("spear") == 0) {
         return new CommonSpear();
     }
+	if (name.compare("dagger") == 0) {
+		return new Dagger();
+	}
+	if (name.compare("CrazyRandomSword") == 0)
+		return new CrazyRandomSword();
+	if (name.compare("hammer") == 0)
+		return new SimpleHammer();
 
     throw "Invalid weapon";
 }
