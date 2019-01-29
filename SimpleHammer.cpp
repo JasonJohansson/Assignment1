98094c94c3b9e1a0cfb3645bc193883acc8081e6
Jason Johansson <jpj16@my.fsu.edu>
2019-01-29 13:02:36 -0500

    Implemented SimpleHammer class prototype and functions

--- SimpleHammer.cpp
+++ SimpleHammer.cpp
@@ -1,1 +1,15 @@
-#pragma once
+//Jason Johansson
+//SimpleHammer.cpp
+
+#include "SimpleHammer.h"
+
+double SimpleHammer::hit(double armor) {
+	double dmg;
+	if (armor < 30)
+		dmg = hitPoints;
+	else
+		dmg = hitPoints - armor;
+	if (dmg < 0)
+		return 0;
+	return dmg;
+}
--- SimpleHammer.h
+++ SimpleHammer.h
@@ -1,1 +1,17 @@
-#pragma once
+//Jason Johansson
+// SimpleHammer Weapon
+
+#ifndef SIMPLEHAMMER_H
+#define SIMPLEHAMMER_H
+
+#include <string>
+#include "Weapon.h"
+
+class SimpleHammer : public Weapon {
+public:
+	SimpleHammer() : Weapon("Simple Hammer", 25) {}
+	virtual ~SimpleHammer() {};
+	virtual double hit(double armor);
+};
+
+#endif 
